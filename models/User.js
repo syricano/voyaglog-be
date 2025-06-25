8fdd55830d1766c9cef16bbbe91927e8c6a79b1d
Anas <syriacano@gmail.com>
2025-06-25 13:15:55 +0200

    add index for better performance

--- models/User.js
+++ models/User.js
@@ -2,33 +2,32 @@ import { Model, DataTypes } from "sequelize";
 import sequelize from '../db/index.js';
 
 const User = sequelize.define('User', {
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
+  firstName: DataTypes.STRING,
+  lastName: DataTypes.STRING,
   username: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true
-  }
-  ,
+    unique: true,
+    // 👇 this ensures an index even if not unique
+    index: true,
+  },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true
+    unique: true,
+    index: true,
   },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  phone: {
-    type: DataTypes.STRING, // optional
-    allowNull: true
-  }
+  password: DataTypes.STRING,
+  phone: DataTypes.STRING,
+}, {
+  indexes: [
+    {
+      fields: ['email'],
+    },
+    {
+      fields: ['username'],
+    },
+  ]
 });
 
 export default User;
